2e7107dff9e1ef9744b7788ca6c9c37e042c2cd9
RyanZhu <zhupeiyan@gmail.com>
2012-10-02 19:22:28 +0800

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,10 +2,25 @@ require 'spec_helper'
 
 describe "StaticPages" do
   describe "Home Page" do
-    it "should have the content 'Sample App'" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+    it "should have the h1 'Sample App'" do
       visit '/static_pages/home'
-      page.should have_content('Sample App')
+      page.should have_selector('h1',:text=>'Sample')
+    end
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      page.should have_selector('title',:text=>'Home')
+    end
+  end
+  describe "Help Page" do
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      page.should have_content('Help')
+    end
+  end
+  describe "About Page" do
+    it "should have the content 'About'" do
+      visit '/static_pages/about'
+      page.should have_content('About')
     end
   end
 end
